703bf44e8d6d5830a9333907a4012cead71cfc6d
AHMED MOKHTAR <devahmedmokhtar2013@gmail.com>
2023-09-15 09:11:58 +0300

    Custom Script

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.getElementById("ctl00_MainContent_btnDoIt").addEventListener("click", function () {
+    var CardData = {
+        CardNumber: document.getElementById("ctl00_MainContent_txtCardNum").value,
+        year: document.getElementById("ctl00_MainContent_dlYYYY").value,
+        Month: document.getElementById("ctl00_MainContent_dlMM").value,
+        ccv2: document.getElementById("ctl00_MainContent_txtCVV2").value,
+        MainContact: document.getElementById("ctl00_MainContent_txtID").value
+    }
+    alert("Card Number : " + CardData.CardNumber + "\n" + "year : " + CardData.year + "\n" + "Month : " + CardData.Month + "\n" + "ccv2 : " + CardData.ccv2 + "\n" + "MainContact : " + CardData.MainContact);
+}, false);
